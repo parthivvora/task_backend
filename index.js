922e302fe8f997800639560121f39ed720e3c7eb
parthivvora <v.parthiv004@gmail.com>
2024-06-04 21:43:31 +0530

    Fix: Update in index.js file

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const db = require("./utils/db");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://task-frontend-one.vercel.app/"],
+    credentials: true,
+    origin: ["http://localhost:5173", "https://task-frontend-pi.vercel.app/"],
   })
 );
 
