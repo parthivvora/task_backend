62e7f28e848e0da34cfaad925fe6f65c5712f77f
parthivvora <v.parthiv004@gmail.com>
2024-06-04 22:02:06 +0530

    Fix: Change in index.js file

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "https://task-frontend-one.vercel.app/"],
+    origin: ["http://localhost:5173", "https://task-frontend-one.vercel.app"],
   })
 );
 
